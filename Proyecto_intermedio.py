43054d6ea787338ef4f969645626122b852a2b2a
apmr11 <124747834+apmr11@users.noreply.github.com>
2023-10-12 15:49:26 -0600

    Funciones

---
+++ Proyecto_intermedio.py
@@ -0,0 +1,46 @@
+class RegEx:
+    def __init__(self):
+        self.text = ""
+
+    def set_text(self, text):
+        """Establece el texto en el que se realizará la coincidencia."""
+        self.text = text
+
+    #Leer el patrón (f/fr, regEx, g/i)
+    def match(self, pattern, text):
+        pass
+
+    #Búsqueda simple
+    def busqueda_simple(self):
+        pass
+
+    #Rango de letras y numeros entre corchetes cuadrados
+    def rangos(self):
+        pass
+
+    #Conjunto de letras entre corchetes
+    def conjuntos(self):
+        pass
+
+    #Letra intermedia antes del signo ? puede o no aparecer en el match encontrado
+    def puede_aparecer(self):
+        pass
+
+    #El operador | funciona como un or lógico. El texto puede hacer match con la string de la izq o der
+    def izq_or_der(self):
+        pass
+
+    #Operador de repetición
+    def repeticion(self):
+        pass
+
+    # Leer funciones y realizar el comodin
+    def leer(self):
+        pass
+
+
+
+
+
+
+
